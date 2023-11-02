690ee83f16d3237c009b6a83833284dfd706f052
Guillermo Puente <guillermo.ps09@gmail.com>
2023-11-02 16:42:09 -0300

    feat(tree-view-input): 🚀 Added TreeViewInput Component

--- src/powerhouse/components/index.ts
+++ src/powerhouse/components/index.ts
@@ -4,4 +4,5 @@ export * from './dropdown-menu';
 export * from './legacy';
 export * from './modal';
 export * from './sidebar';
+export * from './tree-view-input';
 export * from './tree-view-item';
