e9d8e1439847bba5c226e06c7a3a9d394ad9457a
ryanwolhuter <dev@ryanwolhuter.com>
2023-11-30 12:59:09 +0200

    feat: add new modal component
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/powerhouse/components/new-modal/modal.stories.tsx
+++ src/powerhouse/components/new-modal/modal.stories.tsx
@@ -1,5 +1,6 @@
+import { useArgs } from '@storybook/preview-api';
 import { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
+import { DropdownMenu } from '..';
 import { Modal } from './modal';
 
 const meta = {
@@ -16,78 +17,141 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
     args: {
-        children: (
-            <div>
-                lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam quod, voluptatum, quae voluptatem voluptas lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quisquam quod,
-                voluptatum, quae voluptatem voluptas lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Quisquam quod, voluptatum, quae
-                voluptatem voluptaslorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quisquam quod, voluptatum, quae voluptatem
-                voluptaslorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam quod, voluptatum, quae voluptatem voluptaslorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quisquam quod,
-                voluptatum, quae voluptatem voluptaslorem ipsum dolor sit amet
-                consectetur adipisicing elit. Quisquam quod, voluptatum, quae
-                voluptatem voluptaslorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quisquam quod, voluptatum, quae voluptatem
-                voluptaslorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam quod, voluptatum, quae voluptatem voluptaslorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quisquam quod,
-                voluptatum, quae voluptatem voluptaslorem ipsum dolor sit amet
-                consectetur adipisicing elit. Quisquam quod, voluptatum, quae
-                voluptatem voluptaslorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quisquam quod, voluptatum, quae voluptatem
-                voluptaslorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam quod, voluptatum, quae voluptatem voluptaslorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quisquam quod,
-                voluptatum, quae voluptatem voluptaslorem ipsum dolor sit amet
-                consectetur adipisicing elit. Quisquam quod, voluptatum, quae
-                voluptatem voluptaslorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quisquam quod, voluptatum, quae voluptatem
-                voluptaslorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam quod, voluptatum, quae voluptatem voluptaslorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quisquam quod,
-                voluptatum, quae voluptatem voluptaslorem ipsum dolor sit amet
-                consectetur adipisicing elit. Quisquam quod, voluptatum, quae
-                voluptatem voluptaslorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quisquam quod, voluptatum, quae voluptatem
-                voluptaslorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam quod, voluptatum, quae voluptatem voluptaslorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quisquam quod,
-                voluptatum, quae voluptatem voluptaslorem ipsum dolor sit amet
-                consectetur adipisicing elit. Quisquam quod, voluptatum, quae
-                voluptatem voluptaslorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quisquam quod, voluptatum, quae voluptatem
-                voluptaslorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam quod, voluptatum, quae voluptatem voluptaslorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quisquam quod,
-                voluptatum, quae voluptatem voluptaslorem ipsum dolor sit amet
-                consectetur adipisicing elit. Quisquam quod, voluptatum, quae
-                voluptatem voluptaslorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quisquam quod, voluptatum, quae voluptatem
-                voluptaslorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam quod, voluptatum, quae voluptatem voluptaslorem ipsum
-                dolor sit amet consectetur adipisicing elit. Quisquam quod,
-                voluptatum, quae voluptatem voluptas
-            </div>
-        ),
-        isOpen: true,
+        open: true,
     },
     render: function Wrapper(args) {
-        const [isOpen, setIsOpen] = useState(args.isOpen);
+        const [, setArgs] = useArgs<typeof args>();
 
         return (
-            <div className="h-screen w-screen">
+            <div className="h-screen w-screen bg-white">
                 <Modal
                     {...args}
-                    isOpen={isOpen}
-                    onClose={() => setIsOpen(false)}
-                />
+                    open={args.open}
+                    onOpenChange={open => {
+                        setArgs({ ...args, open });
+                    }}
+                >
+                    <div>
+                        <DropdownMenu
+                            onItemClick={id => alert(id)}
+                            className="mb-3 block items-center justify-center rounded bg-blue-500 text-white"
+                            menuClassName="border-2 border-indigo-600 w-64 rounded bg-white cursor-pointer"
+                            menuItemClassName="hover:bg-gray-200 px-2"
+                            items={[
+                                {
+                                    id: 'item-1',
+                                    content: 'Item 1',
+                                },
+                                {
+                                    id: 'item-2',
+                                    content: 'Item 2',
+                                },
+                                {
+                                    id: 'item-3',
+                                    content: 'Item 3',
+                                },
+                            ]}
+                        >
+                            open menu
+                        </DropdownMenu>
+                        lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam quod, voluptatum, quae voluptatem voluptas
+                        lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam quod, voluptatum, quae voluptatem voluptas
+                        lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam quod, voluptatum, quae voluptatem voluptaslorem
+                        ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam quod, voluptatum, quae voluptatem voluptaslorem
+                        ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam quod, voluptatum, quae voluptatem voluptaslorem
+                        ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam quod, voluptatum, quae voluptatem voluptaslorem
+                        ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam quod, voluptatum, quae voluptatem voluptaslorem
+                        ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam quod, voluptatum, quae voluptatem voluptaslorem
+                        ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam quod, voluptatum, quae voluptatem
+                        voluptasloremquae voluptatem voluptaslorem ipsum dolor
+                        sit amet consectetur adipisicing elit. Quisquam quod,
+                        voluptatum, quae voluptatem voluptaslorem ipsum dolor
+                        sit amet consectetur adipisicing elit. Quisquam quod,
+                        voluptatum, quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptasloremquae voluptatem
+                        voluptaslorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam quod, voluptatum, quae
+                        voluptatem voluptaslorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quisquam quod, voluptatum,
+                        quae voluptatem voluptaslorem
+                    </div>
+                </Modal>
                 <button
                     onClick={() => {
-                        setIsOpen(true);
+                        setArgs({ ...args, open: true });
                     }}
                 >
                     Open modal
--- src/powerhouse/components/new-modal/modal.tsx
+++ src/powerhouse/components/new-modal/modal.tsx
@@ -1,75 +1,24 @@
-import {
-    ComponentPropsWithoutRef,
-    useCallback,
-    useEffect,
-    useRef,
-    type MouseEvent,
-    type ReactNode,
-} from 'react';
-import { twMerge } from 'tailwind-merge';
+import { Content, Overlay, Portal, Root } from '@radix-ui/react-dialog';
 
-export type ModalProps = {
-    children: ReactNode;
-    isOpen: boolean;
-    onClose?: () => void;
-    dialogProps?: ComponentPropsWithoutRef<'dialog'>;
+type Props = {
+    children?: React.ReactNode;
+    open?: boolean;
+    onOpenChange?: (open: boolean) => void;
 };
-
-export function Modal(props: ModalProps) {
-    const dialogRef = useRef<HTMLDialogElement>(null);
-    function showModal() {
-        if (!dialogRef.current) return;
-        dialogRef.current.showModal();
-    }
-
-    const closeModal = useCallback(() => {
-        if (!dialogRef.current) return;
-        dialogRef.current.close();
-        props.onClose?.();
-    }, [props]);
-    useEffect(() => {
-        if (!dialogRef.current) return;
-        if (props.isOpen) showModal();
-        else closeModal();
-    }, [props.isOpen, closeModal]);
-
-    function onClick(event: MouseEvent) {
-        if (!dialogRef.current) return;
-        if (isClickOnBackdrop(event)) closeModal();
-    }
-
-    function isClickOnBackdrop(event: MouseEvent) {
-        if (!dialogRef.current) return false;
-        if (event.target !== dialogRef.current) return false;
-        const boundingRect = dialogRef.current.getBoundingClientRect();
-        return !(
-            event.clientX < boundingRect.right &&
-            event.clientX > boundingRect.left &&
-            event.clientY > boundingRect.top &&
-            event.clientY < boundingRect.bottom
-        );
-    }
-
+export function Modal(props: Props) {
     return (
-        <>
-            <div
-                className={twMerge(
-                    'pointer-events-none fixed h-screen w-screen overflow-hidden bg-slate-900/0 transition-colors duration-1000',
-                    props.isOpen && 'bg-slate-900/50',
-                )}
-            />
-            <dialog
-                ref={dialogRef}
-                onClick={onClick}
-                {...props.dialogProps}
-                className={twMerge(
-                    props.dialogProps?.className,
-                    'fixed grid place-items-center overflow-hidden transition duration-1000 backdrop:bg-transparent open:overflow-y-scroll',
-                    props.isOpen ? 'opacity-100' : 'opacity-0',
-                )}
-            >
-                {props.children}
-            </dialog>
-        </>
+        <Root
+            open={props.open}
+            defaultOpen={props.open}
+            onOpenChange={props.onOpenChange}
+        >
+            <Portal>
+                <Overlay className="fixed inset-0 grid place-items-center overflow-y-auto bg-slate-900/50 data-[state=closed]:animate-fade-out data-[state=open]:animate-fade-in">
+                    <Content className="isolate -z-10 min-w-[300px] max-w-[400px] rounded-xl bg-white p-6 data-[state=closed]:animate-zoom-out data-[state=open]:animate-zoom-in">
+                        {props.children}
+                    </Content>
+                </Overlay>
+            </Portal>
+        </Root>
     );
 }
