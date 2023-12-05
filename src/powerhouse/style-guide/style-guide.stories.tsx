40b934fe05feb96caa5f2411c392574d5b826a7c
ryanwolhuter <dev@ryanwolhuter.com>
2023-12-05 14:09:39 +0200

    feat: show hex values
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/powerhouse/style-guide/style-guide.stories.tsx
+++ src/powerhouse/style-guide/style-guide.stories.tsx
@@ -8,20 +8,42 @@ const meta = {
 
 export default meta;
 
+function hslToHex(hsl: string) {
+    const match = hsl.match(/\d+/g);
+    if (!match) return;
+    const [h, s, l] = match.map(Number);
+    const lScaled = l / 100;
+    const a = (s * Math.min(lScaled, 1 - lScaled)) / 100;
+    const f = (n: number) => {
+        const k = (n + h / 30) % 12;
+        const color = lScaled - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color)
+            .toString(16)
+            .padStart(2, '0');
+    };
+
+    return `#${f(0)}${f(8)}${f(4)}`.toUpperCase();
+}
+
 export const Colors: StoryObj<typeof meta> = {
     render: function Wrapper() {
         return (
             <div className="mx-auto flex max-w-[650px] flex-wrap gap-1 bg-white p-6">
                 {Object.keys(twConfig.theme.colors).map(key => {
-                    const colorVar = `var(--${key})`;
+                    const colorVar = `--${key}`;
+                    const colorValue = getComputedStyle(
+                        document.documentElement,
+                    ).getPropertyValue(colorVar);
 
                     return (
                         <div
                             key={key}
                             className="grid aspect-square w-[30%] max-w-[200px] place-items-center"
-                            style={{ backgroundColor: colorVar }}
+                            style={{ backgroundColor: `var(${colorVar})` }}
                         >
-                            <div className="bg-white p-1 text-xs">{key}</div>
+                            <div className="bg-white p-1 text-xs">
+                                {key} {hslToHex(colorValue)}
+                            </div>
                         </div>
                     );
                 })}
@@ -45,7 +67,7 @@ export const TextSizes: StoryObj<typeof meta> = {
         ];
 
         return (
-            <div className="mx-auto flex max-w-[720px] flex-wrap gap-1 bg-white p-6">
+            <div className="mx-auto flex max-w-[650px] flex-wrap gap-1 bg-white p-6">
                 {textSizes.map(key => {
                     return (
                         <p key={key} className={twJoin(key, 'mb-2')}>
