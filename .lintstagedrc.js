ad00a36e2be18cb6947ab453df0f153b91338d04
ryanwolhuter <dev@ryanwolhuter.com>
2024-01-17 10:45:03 +0200

    feat: update lint staged commands
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,7 @@
 export default {
-    './**/*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix'],
+    './**/*.{js,jsx,ts,tsx,json}': [
+        'yarn lint',
+        'vitest related --run',
+        'yarn format',
+    ],
 };
