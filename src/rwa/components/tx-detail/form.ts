fc3d813df72a3a044bd51a3ab054e0e37ec71e5f
ryanwolhuter <dev@ryanwolhuter.com>
2024-03-11 13:32:58 +0200

    feat: rename for clarity
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/rwa/components/tx-detail/form.ts
+++
@@ -1,4 +0,0 @@
-export type RWATxSelectTypes = 'assetTypeId' | 'cusipIsinAssetNameId';
-export type RWATxTextTypes = 'transactionType' | 'assetProceedsUSD';
-export type RWATxDateTypes = 'timestamp';
-export type RWATxFeesTypes = 'feesTable';
