4b9076ad1a47e00be529c438ef935fcbfa2c659c
ryanwolhuter <dev@ryanwolhuter.com>
2024-06-27 15:28:45 +0200

    feat: add parent folder to tree item
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/connect/types/ui.ts
+++ src/connect/types/ui.ts
@@ -31,6 +31,7 @@ export type BaseTreeItem = {
     label: string;
     type: TreeItemType;
     availableOffline: boolean;
+    parentFolder: string | null;
     syncStatus?: SyncStatus;
     error?: Error;
     options?: ConnectDropdownMenuItem[];
