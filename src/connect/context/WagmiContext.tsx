33702f53a0612a17d1b2fc5e4de8253d1580f669
ryanwolhuter <dev@ryanwolhuter.com>
2024-07-01 17:18:59 +0200

    feat: allow consumer to pass provider props
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/connect/context/WagmiContext.tsx
+++ src/connect/context/WagmiContext.tsx
@@ -1,14 +1,29 @@
-import { wagmiConfig } from '@/services';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { wagmiConfig as defaultWagmiConfig } from '@/services';
+import {
+    QueryClient,
+    QueryClientProvider,
+    QueryClientProviderProps,
+} from '@tanstack/react-query';
 import { ReactNode } from 'react';
-import { WagmiProvider } from 'wagmi';
+import { WagmiProvider, WagmiProviderProps } from 'wagmi';
 
-const queryClient = new QueryClient();
+const defaultQueryClient = new QueryClient();
 
-export function WagmiContext({ children }: { children: ReactNode }) {
+type Props = {
+    wagmiProviderProps?: WagmiProviderProps;
+    queryClientProviderProps?: QueryClientProviderProps;
+    children: ReactNode;
+};
+export function WagmiContext(props: Props) {
+    const { children, wagmiProviderProps, queryClientProviderProps } = props;
+    const { config = defaultWagmiConfig, ...wagmiProps } =
+        wagmiProviderProps ?? {};
+    const { client = defaultQueryClient, ...queryClientProps } =
+        queryClientProviderProps ?? {};
+
     return (
-        <WagmiProvider config={wagmiConfig}>
-            <QueryClientProvider client={queryClient}>
+        <WagmiProvider config={config} {...wagmiProps}>
+            <QueryClientProvider client={client} {...queryClientProps}>
                 {children}
             </QueryClientProvider>
         </WagmiProvider>
